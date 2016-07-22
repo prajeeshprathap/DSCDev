b3d06e0ca01c1f98e753da3184bed10d3e738298
Prajeeshp <Prajeeshp@PRAJEESHP01>
2016-07-22 16:45:35 +0200

    allowunsecureconnection

--- PullClient/ClientConfig.ps1
+++ PullClient/ClientConfig.ps1
@@ -15,8 +15,9 @@ configuration PullClientConfiguration
             ServerURL          = 'http://52.174.151.121/psdscpullserver.svc/'
             RegistrationKey    = 'c944ce11-0ffe-467b-bb22-fd1cd2fd76bc'
             ConfigurationNames = @('ClientConfig')
-        }   
-
+            AllowUnsecureConnection = $true #This needs to be fixed in the future.
+        }  
+        
         ReportServerWeb EndeavourPullServer
         {
             ServerURL       = 'http://52.174.151.121/psdscpullserver.svc/'
@@ -26,4 +27,4 @@ configuration PullClientConfiguration
 }
 
 PullClientConfiguration
-Start-DscConfiguration -Path .\PullClientConfiguration -Wait -Verbose
+Set-DscLocalConfigurationManager -Path .\PullClientConfiguration -Verbose
