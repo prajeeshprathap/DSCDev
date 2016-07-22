4f8a9f43579232de75070dd89018fd8fcc187158
Prajeeshp <Prajeeshp@PRAJEESHP01>
2016-07-22 17:06:15 +0200

    removed configuration names

--- PullClient/ClientConfig.ps1
+++ PullClient/ClientConfig.ps1
@@ -15,7 +15,6 @@ configuration PullClientConfiguration
         {
             ServerURL          = 'http://52.174.151.121/psdscpullserver.svc/'
             RegistrationKey    = 'c944ce11-0ffe-467b-bb22-fd1cd2fd76bc'
-            ConfigurationNames = @('ClientConfig')
             AllowUnsecureConnection = $true #This needs to be fixed in the future.
         }  
 
