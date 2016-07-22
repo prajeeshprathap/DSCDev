274009efecb854aa1a66cc6c5ecdf54f1fbb3074
Prajeeshp <Prajeeshp@PRAJEESHP01>
2016-07-22 16:47:09 +0200

    allow unsecure connection

--- PullClient/ClientConfig.ps1
+++ PullClient/ClientConfig.ps1
@@ -22,7 +22,7 @@ configuration PullClientConfiguration
         {
             ServerURL       = 'http://52.174.151.121/psdscpullserver.svc/'
             RegistrationKey = 'c944ce11-0ffe-467b-bb22-fd1cd2fd76bc'
-            AllowUnsecureConnection = $true #This needs to be fixed in the future.
+            AllowUnsecureConnection = $true 
         }
     }
 }
