8300a4d0296fcfb44896519051e6d3c3752f6e0b
Prajeeshp <Prajeeshp@PRAJEESHP01>
2016-07-22 16:46:39 +0200

    report server allow unsecure connection

--- PullClient/ClientConfig.ps1
+++ PullClient/ClientConfig.ps1
@@ -17,11 +17,12 @@ configuration PullClientConfiguration
             ConfigurationNames = @('ClientConfig')
             AllowUnsecureConnection = $true #This needs to be fixed in the future.
         }  
-        
+
         ReportServerWeb EndeavourPullServer
         {
             ServerURL       = 'http://52.174.151.121/psdscpullserver.svc/'
             RegistrationKey = 'c944ce11-0ffe-467b-bb22-fd1cd2fd76bc'
+            AllowUnsecureConnection = $true #This needs to be fixed in the future.
         }
     }
 }
