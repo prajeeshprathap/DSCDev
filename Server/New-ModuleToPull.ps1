2fdce33439bee46928865b6c960b4e601a38d523
Prajeeshp <Prajeeshp@PRAJEESHP01>
2016-07-26 11:34:53 +0200

    updated module and dsc resource

--- Server/New-ModuleToPull.ps1
+++ Server/New-ModuleToPull.ps1
@@ -1,14 +1,14 @@
 $parentFolder = Split-Path -Parent (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
-$testModulePath = Join-Path $parentFolder 'Modules\xWindowsUpdate'
+$testModulePath = Join-Path $parentFolder 'Modules\xDismFeature'
 
-"Compressing the module xTestResource to $env:TEMP\xWindowsUpdate_1.0.zip" | Write-Host
+"Compressing the module xTestResource to $env:TEMP\xDismFeature_1.0.zip" | Write-Host
 Add-Type -A System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::CreateFromDirectory($testModulePath, "$env:TEMP\xWindowsUpdate_1.0.zip")
+[IO.Compression.ZipFile]::CreateFromDirectory($testModulePath, "$env:TEMP\xDismFeature_1.0.zip")
 
 
 $moduleUploadFolder = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\"
 "Moving the compressed folder to module share location $moduleUploadFolder" | Write-Host
-Move-Item -Path $env:TEMP\xWindowsUpdate_1.0.zip -Destination "$moduleUploadFolder"
+Move-Item -Path $env:TEMP\xDismFeature_1.0.zip -Destination "$moduleUploadFolder"
 
 "Creating checksum" | Write-Host
 New-DSCCheckSum -path $moduleUploadFolder -force
