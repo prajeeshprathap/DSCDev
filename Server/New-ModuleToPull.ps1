b01e981a8b5e4f63db3906aeb8be1e3e3c4a6957
Prajeeshp <Prajeeshp@PRAJEESHP01>
2016-07-26 11:23:20 +0200

    added test module

--- Server/New-ModuleToPull.ps1
+++ Server/New-ModuleToPull.ps1
@@ -1,14 +1,14 @@
 $parentFolder = Split-Path -Parent (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)
-$testModulePath = Join-Path $parentFolder 'DSCResources\xTestResource'
+$testModulePath = Join-Path $parentFolder 'Modules\PSVirtualBox'
 
-"Compressing the module xTestResource to $env:TEMP\xTestResource_1.0.zip" | Write-Host
+"Compressing the module xTestResource to $env:TEMP\PSVirtualBox_1.0.zip" | Write-Host
 Add-Type -A System.IO.Compression.FileSystem
-[IO.Compression.ZipFile]::CreateFromDirectory($testModulePath, "$env:TEMP\xTestResource_1.0.zip")
+[IO.Compression.ZipFile]::CreateFromDirectory($testModulePath, "$env:TEMP\PSVirtualBox_1.0.zip")
 
 
 $moduleUploadFolder = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\"
 "Moving the compressed folder to module share location $moduleUploadFolder" | Write-Host
-Move-Item -Path $env:TEMP\xTestResource_1.0.zip -Destination "$moduleUploadFolder"
+Move-Item -Path $env:TEMP\PSVirtualBox_1.0.zip -Destination "$moduleUploadFolder"
 
 "Creating checksum" | Write-Host
 New-DSCCheckSum -path $moduleUploadFolder -force
