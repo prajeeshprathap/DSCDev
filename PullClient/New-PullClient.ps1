c1d0a9c19533e45c47d77cbd4ad6703832002698
Prajeeshp <Prajeeshp@PRAJEESHP01>
2016-07-26 11:39:32 +0200

    fixed a bug incode

--- PullClient/New-PullClient.ps1
+++ PullClient/New-PullClient.ps1
@@ -14,11 +14,11 @@ $configServerKey = 'c944ce11-0ffe-467b-bb22-fd1cd2fd76bc'
 $reportServerKey = '62f25315-62f1-47eb-a091-fc1f7b70f40e'
 
 .\ClientConfig.ps1 -ConfigurationServerUrl $configServerUrl `
-                    $ModuleServerUrl $configServerUrl `
-                    $ReportServerUrl $reportServerUrl `
-                    $ConfigurationServerKey $configServerKey `
-                    $ModuleServerKey $configServerKey `
-                    $ReportServerKey $reportServerKey
+                    -ModuleServerUrl $configServerUrl `
+                    -ReportServerUrl $reportServerUrl `
+                    -ConfigurationServerKey $configServerKey `
+                    -ModuleServerKey $configServerKey `
+                    -ReportServerKey $reportServerKey
 
 
 Set-DscLocalConfigurationManager -Path .\PullClientConfiguration -Verbose -Force 
